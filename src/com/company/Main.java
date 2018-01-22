a693b2ae5ecdeba8a9dad05f05455694cbc1987b
ms787 <jajalope372@gmail.com>
2018-01-22 08:51:28 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Long fib[];
+
+
+    }
+    public static void fibonacciCheck (Long checked, Long temp1, Long temp2) {
+        if (checked == temp1 ||)
+    }
+}
